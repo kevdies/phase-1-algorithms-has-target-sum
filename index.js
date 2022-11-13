8c59cddacf932d6ab514903552e5e092e5ee39e2
kevin diesenberg <kdiesenb@gmail.com>
2022-11-13 09:10:03 -0500

    commit tree bef5e9cbd10fd08f873e404e6d4f070e5a1a7d3d

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (j + i === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
-/* 
+/*
   Write the Big O time complexity of your function here
 */
 
-/* 
+/*
   Add your pseudocode here
 */
 
